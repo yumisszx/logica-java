f7e184be5e182a125326a5bbb34e3fce2b26ed4e
unknown <ligiayumimeira@gmail.com>
2026-01-28 21:50:58 -0300

    codigo multiplos de 5 ate o 100

---
+++ condicional-repeticao/MultiplosDe5.java
@@ -0,0 +1,12 @@
+//Imprimir múltiplos de 5 até 100
+//repetição para mostrar de 5 em 5 até 100 (sem %)
+
+public class MultiplosDe5 {
+    public static void main(String[] args) {
+        //laço para imprimir múltiplos de 5 até 100
+        System.out.println("Múltiplos de 5 até 100:");
+        for (int i = 5; i <= 100; i += 5) {
+            System.out.println(i); //print do múltiplo de 5
+        }
+    }
+}
